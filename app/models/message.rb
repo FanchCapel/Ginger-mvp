b42771d8781bc60a053671df4c739f3c26b4a838
Francois Capel <fanch.capel@gmail.com>
2020-03-11 11:58:07 +0100

    messages en mode production

--- app/models/message.rb
+++ app/models/message.rb
@@ -13,17 +13,17 @@ class Message < ApplicationRecord
   private
 
   # SENDING MESSAGES FOR PRODUCTION
-  # def plan_message
-  #   send_date = (self.experience.date + self.message_type.day).beginning_of_day
-  #   send_date = send_date + self.message_type.send_at.hour.hours
-  #   message = self
-  #   p eval message_type.content
-  #   MessageJob.set(wait_until: send_date).perform_later(self.id)
-  # end
+  def plan_message
+    send_date = (self.experience.date + self.message_type.day).beginning_of_day
+    send_date = send_date + self.message_type.send_at.hour.hours
+    message = self
+    p eval message_type.content
+    MessageJob.set(wait_until: send_date).perform_later(self.id)
+  end
 
   # SENDING MESSAGES FOR DEMO
-  def plan_message
-    send_date = message_type.day * 20
-    MessageJob.set(wait: send_date.seconds).perform_later(self.id)
-  end
+  # def plan_message
+  #   send_date = message_type.day * 20
+  #   MessageJob.set(wait: send_date.seconds).perform_later(self.id)
+  # end
 end
